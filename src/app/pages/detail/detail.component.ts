751c02635c0d25b3c8deb35b46ebcee01647df7c
Antonio Gantolea <antoniogantolea@yahoo.it>
2024-05-24 12:25:45 +0200

    secondo pezzo

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { iAuto } from '../../models/i-auto';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.scss'
+  styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent {
 
+  autoArr: iAuto[] = [];
+  currentAuto!: iAuto;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  async ngOnInit() {
+    await this.getAuto();
+    this.route.params.subscribe(params => {
+      const model = params['model'];
+      const autoTrovata = this.autoArr.find(auto => auto.model === model);
+
+      if (autoTrovata) {
+        this.currentAuto = autoTrovata;
+      } else {
+        console.error('auto non trovata');
+      }
+    });
+  }
+
+  async getAuto(){
+    const response = await fetch('../../../assets/db.json');
+    const modelli = <iAuto[]> await response.json();
+    this.autoArr = modelli;
+  }
 }
